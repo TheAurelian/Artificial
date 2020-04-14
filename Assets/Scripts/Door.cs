d57cfbcc53db3425209bc3f2b736989b7579d5c2
Luke McPherson <TheAurelian@users.noreply.github.com>
2020-04-14 04:20:41 -0500

    Unable to sleep so textures and locked doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Door : InteractiveObject
 {
+    [Tooltip("Check this box to lock the door")]
+    [SerializeField]
+    private bool isLocked;
+
+    [Tooltip("The text displayed if the door is locked")]
+    [SerializeField]
+    private string lockedDisplayText = "Locked Door";
+
+    [Tooltip("Plays when the player interacts with a locked door without owning the key")]
+    [SerializeField]
+    private AudioClip lockedAudioClip;
+
+    [Tooltip("Plays when the player opens the door")]
+    [SerializeField]
+    private AudioClip openAudioClip;
+
+    public override string DisplayText => isLocked ? lockedDisplayText : base.DisplayText;
+
     private Animator animator;
     private bool isOpen = false;
+    
 
     /// <summary>
     /// Using a constructor her to initialize displayText in the editor
@@ -27,10 +46,22 @@ public class Door : InteractiveObject
     {
         if (!isOpen)
         {
-            base.InteractiveWith();
-            animator.SetBool("shouldOpen", true);
-            displayText = string.Empty;
-            isOpen = true;
+            if (!isLocked)
+            {
+                audioSource.clip = openAudioClip;
+                animator.SetBool("shouldOpen", true);
+                displayText = string.Empty;
+                isOpen = true;
+            }
+
+            // If the door is locked set audio to play this sound
+            else
+            {
+                audioSource.clip = lockedAudioClip;
+            }
+
+            // This plays the sound effect
+            base.InteractiveWith(); 
         }
     }
 }
---
+++ Assets/Scripts/RotatePuzzle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RotatePuzzle : InteractiveObject
+{
+    private Animator animator;
+    private bool isRotate = false;
+
+    /// <summary>
+    /// Using a constructor here to initialize displayText in the editor
+    /// </summary>
+
+    public RotatePuzzle()
+    {
+        displayText = nameof(RotatePuzzle);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
+
+    public override void InteractiveWith()
+    {
+        if (!isRotate)
+        {
+            // This plays a sound effect
+            base.InteractiveWith();
+            animator.SetBool("shouldRotate", true);
+            isRotate = true;
+        }
+        
+    }
+
+    public void Update()
+    {
+        animator.SetBool("shouldRotate", false);
+        isRotate = false;
+    }
+}
+
