6322b99e2f78aa51515fe74f62cb06ceb48c5a74
Luke McPherson <TheAurelian@users.noreply.github.com>
2020-04-28 13:23:46 -0500

    Tiny polish and broke puzzle door
    Redoing the puzzle door

---
+++ Assets/Scripts/DroneMovePath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovePath : MonoBehaviour
+{
+    [Tooltip("The speed of the drone")]
+    [SerializeField]
+    private int movementSpeed;
+
+    private void Start()
+    {
+        StartCoroutine(Begin());
+    }
+
+    IEnumerator Begin()
+    {
+        float time = 1;
+        while (time > 0) 
+        {
+            StartCoroutine(DroneMovement());
+            yield return new WaitForSeconds(10);
+        }
+    }
+
+    IEnumerator DroneMovement()
+    {
+        float timePassed = 0;
+        while (timePassed < 7)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+            Debug.Log("Drone is moving left");
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+        transform.Rotate(0, 180, 0);
+    }
+}
