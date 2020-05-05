d2650ab9b5c5536e7ef1f051d8ad96de4d9eb17a
Luke McPherson <TheAurelian@users.noreply.github.com>
2020-05-05 13:43:02 -0500

    More polish

---
+++ Assets/Scripts/MoveUp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour
+{
+    [Tooltip("The speed of the platform")]
+    [SerializeField]
+    private int movementSpeed;
+
+    [Tooltip("The movement time of the platform")]
+    [SerializeField]
+    private int movementTime;
+
+    private void Start()
+    {
+        StartCoroutine(Begin());
+    }
+
+    IEnumerator Begin()
+    {
+        float time = 1;
+        while (time > 0)
+        {
+            StartCoroutine(PlatformUp());
+            yield return new WaitForSeconds(14);
+        }
+    }
+
+    IEnumerator PlatformUp()
+    {
+        Debug.Log("Platform Up");
+        float timePassed = 0;
+        while (timePassed < movementTime)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * movementSpeed);
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+        StartCoroutine(PlatformDown());
+    }
+
+    IEnumerator PlatformDown()
+    {
+        Debug.Log("Platform Down");
+        float timePassed = 0;
+        while (timePassed < movementTime)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * movementSpeed);
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/PuzzleToggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleToggle : InteractiveObject
+{
+    [Tooltip("The GameObject to toggle")]
+    [SerializeField]
+    private GameObject objectToToggle;
+
+    [Tooltip("The reverse GameObject to toggle")]
+    [SerializeField]
+    private GameObject objectBToToggle;
+
+    [Tooltip("Can the player interact with this more than once")]
+    [SerializeField]
+    private bool isReusable = true;
+
+    private bool hasBeenUsed = false;
+
+    /// <summary>
+    /// Toggles the activeSelf value for the objectToToggle when the player interacts with this item.
+    /// </summary>
+
+    public override void InteractiveWith()
+    {
+        if (isReusable || !hasBeenUsed)
+        {
+            base.InteractiveWith();
+            objectToToggle.SetActive(!objectToToggle.activeSelf);
+            objectBToToggle.SetActive(!objectBToToggle.activeSelf);
+            hasBeenUsed = true;
+
+            if (!isReusable) displayText = string.Empty;
+        }
+    }
+}
